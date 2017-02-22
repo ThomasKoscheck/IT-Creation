f60e432faff9b2dd154cb42ec4217d25c65c7d01
Thomas K <thomas.koscheck@protonmail.ch>
2017-02-22 07:48:57 +0100

    first try

--- main.ino
+++ main.ino
@@ -1,25 +1,20 @@
-// Adafruit Motor shield library
-// copyright Adafruit Industries LLC, 2009
-// this code is public domain, enjoy!
-
 #include <AFMotor.h>
-
-AF_DCMotor motor(1);
+AF_DCMotor motorlinks(1);
+AF_DCMotor motorrechts(2);
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
-  Serial.println("Motor test!");
 
   // turn on motor
-  motor.setSpeed(200);
- 
-  motor.run(RELEASE);
+  motorlinks.setSpeed(200);
+  motorrechts.setSpeed(200);
+  motorlinks.run(RELEASE);
+  motorrechts.run(RELEASE);
 }
 
 void loop() {
   uint8_t i;
   
-  Serial.print("tick");
   
   motor.run(FORWARD);
   for (i=0; i<255; i++) {
