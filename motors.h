77d2cae836224b6840d1347c93d24de80e8580bd
Thomas K <thomas.koscheck@protonmail.ch>
2017-02-22 08:00:26 +0100

    anpassung an dc motoren

--- motors.h
+++ motors.h
@@ -1,51 +1,40 @@
 #ifndef
+#include <AFMotor.h>
 
-AF_DCMotor motorlinks(1);
-AF_DCMotor motorrechts(2);
+AF_DCMotor motorLinks(1);
+AF_DCMotor motorRechts(2);
 
-void Vorwaertsfahren(int steps = 1) {
-    setStepCount(steps+10);
-    linkerMotor->run(FORWARD);
-    rechterMotor->run(FORWARD);
-    while(testStep())
-    {
-        delayMicroseconds(10);
-    }
+void Vorwaertsfahren(int steps = 10) {
+    motorLinks.run(FORWARD);
+    motorRechts.run(FORWARD);
 }
 
-void Linksfahren(int steps = 1) {
-    setStepCount(steps+10);
-    linkerMotor->run(BACKWARD);
-    rechterMotor->run(FORWARD);
-    while(testStep())
-    {
-        delayMicroseconds(10);
-    }
+void Linksfahren(int steps = 10) {
+    motorLinks.run(BACKWARD);
+    motorRechts.run(FORWARD);
 }
 
-void Rechtsfahren(int steps = 1) {
-    setStepCount(steps+10);
-    linkerMotor->run(FORWARD);
-    rechterMotor->run(BACKWARD);
-    while(testStep())
-    {
-        delayMicroseconds(10);
-    }
+void Rechtsfahren(int steps = 10) {
+    motorLinks.run(FORWARD);
+    motorRechts.run(BACKWARD);
 }
 
-void Zurueckfahren(int steps = 1) {
-    setStepCount(steps+10);
-    linkerMotor->run(BACKWARD);
-    rechterMotor->run(BACKWARD);
-    while(testStep())
-    {
-        delayMicroseconds(10);
-    }
+void Zurueckfahren(int steps = 10) {
+    motorLinks.run(BACKWARD);
+    motorRechts.run(BACKWARD)
 }
 
 void MotorStop() {
-   linkerMotor->run(RELEASE);
-   rechterMotor->run(RELEASE);
+   motorLinks.run(RELEASE);
+   motorRechts.run(RELEASE);
+}
+
+void setSpeedLeft(uint16_t speed = 200)  {
+    motorLinks.setSpeed(speed);
+}
+
+void setSpeedRight(uint16_t speed = 200)    {
+    motorRechts.setSpeed(speed);
 }
 
 #endif
